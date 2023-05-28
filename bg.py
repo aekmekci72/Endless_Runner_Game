0b7a1a912b892938e8fe8b058b26ec68a5baaa87
aekmekci72 <anna.ekmekci@gmail.com>
2023-05-28 16:23:16 -0400

    animation bc why not (STANDIN PERSON YOU CAN CHANGE IF YOU WANT)

--- bg.py
+++ bg.py
@@ -22,6 +22,10 @@ obslist=[spike1,spike3]
 
 scroll = 0
 tiles = math.ceil(SCREEN_WIDTH  / bg_width) + 1
+indexon=0
+megacount=0
+jumpon=0
+jumpcount=0
 
 class Obstacle(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -36,9 +40,28 @@ class Obstacle(pygame.sprite.Sprite):
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.standing_surface = pygame.transform.scale(pygame.image.load("images/char_stand.png"), (48, 64))
-        self.jumping_surface = pygame.transform.scale(pygame.image.load("images/char_jump.png"), (48, 64))
-        self.image = self.standing_surface
+        # self.standing_surface = pygame.transform.scale(pygame.image.load("images/char_stand.png"), (48, 64))
+        # self.jumping_surface = pygame.transform.scale(pygame.image.load("images/char_jump.png"), (48, 64))
+
+        
+        self.a1 = pygame.transform.scale(pygame.image.load("images/a1.png"), (48, 64))
+        self.a2 = pygame.transform.scale(pygame.image.load("images/a2.png"), (48, 64))
+        self.a3 = pygame.transform.scale(pygame.image.load("images/a3.png"), (48, 64))
+        self.a4 = pygame.transform.scale(pygame.image.load("images/a4.png"), (48, 64))
+        self.a5 = pygame.transform.scale(pygame.image.load("images/a5.png"), (48, 64))
+        self.a6 = pygame.transform.scale(pygame.image.load("images/a6.png"), (48, 64))
+        self.a7 = pygame.transform.scale(pygame.image.load("images/a7.png"), (48, 64))
+        self.a8 = pygame.transform.scale(pygame.image.load("images/a8.png"), (48, 64))
+        self.animlist=[self.a1,self.a2,self.a3,self.a4,self.a5,self.a6,self.a7,self.a8]
+
+        self.j1 = pygame.transform.scale(pygame.image.load("images/j1.png"), (48, 64))
+        self.j2 = pygame.transform.scale(pygame.image.load("images/j2.png"), (36, 48))
+        self.j3 = pygame.transform.scale(pygame.image.load("images/j3.png"), (48, 64))
+        self.jumplist=[self.j1,self.j2,self.j3]
+
+
+
+        self.image = self.animlist[0]
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -53,15 +76,16 @@ class Player(pygame.sprite.Sprite):
             self.rect.y = SCREEN_HEIGHT - 65
             self.velocity_y = 0
             self.is_jumping = False
+            jumpcount=0
 
         die = pygame.sprite.spritecollide(self, obstacle_group, False)
         if die:
             run = False
 
         if self.is_jumping:
-            self.image = self.jumping_surface
+            self.image = self.jumplist[jumpon]
         else:
-            self.image = self.standing_surface
+            self.image = self.animlist[indexon]
 obstacle_group = pygame.sprite.Group()
 run = True
 freq=250
@@ -71,6 +95,19 @@ player = Player(50, SCREEN_HEIGHT - 65)
 player_group = pygame.sprite.Group()
 player_group.add(player)
 while run:
+    if player.is_jumping:
+        jumpcount+=1
+        if jumpcount<=7:
+            jumpon=0
+        elif jumpcount<=30:
+            jumpon=1
+        else:
+            jumpon=2
+    megacount+=1
+    if(megacount%5==0):
+        indexon+=1
+        if indexon==8:
+            indexon=0
     
     clock.tick(FPS)
     
@@ -103,6 +140,7 @@ while run:
             if event.key == pygame.K_UP and not player.is_jumping: 
                 player.velocity_y = JUMP_FORCE
                 player.is_jumping = True
+                jumpcount=0
         if event.type==pygame.MOUSEBUTTONDOWN:
             position=pygame.mouse.get_pos()
             print("mouse clicked")
