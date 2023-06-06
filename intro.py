9fcc8fab909b524b96d7ce02db976fc05f2b89a6
aekmekci72 <anna.ekmekci@gmail.com>
2023-06-06 10:51:09 -0400

    because why not

--- intro.py
+++ intro.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 import os
 import subprocess
@@ -24,14 +25,48 @@ button_height = 50
 button_padding = 20
 button_start_y = (screen_height - (len(games) * (button_height + button_padding))) // 2
 
-background_color = (0, 0, 0)
-button_color = (255, 255, 255)
-text_color = (0, 0, 0)
+background_color = (225,225,225)
+button_color = (0, 0, 0)
+text_color = (225,225, 225)
 
 
+class Ball:
+    def __init__(self):
+        self.radius = 20
+        self.color = (255, 0, 0)  
+        self.x = random.randint(self.radius, screen_width - self.radius)
+        self.y = -self.radius
+        self.velocity = 0
+        self.gravity = 0.5
+
+    def update(self):
+        self.y += self.velocity
+        self.velocity += self.gravity
+
+        if self.y + self.radius >= screen_height:
+            self.y = screen_height - self.radius
+            self.velocity *= -0.8
+
+            if abs(self.velocity) < 5:
+                self.y=screen_height-15
+
+    def draw(self):
+        pygame.draw.circle(screen, self.color, (self.x, int(self.y)), self.radius)
+
+balls=[]
 
 running = True
+
+clock = pygame.time.Clock()
+
+freq=200
+
+
 while running:
+    r=random.randint(0,freq)
+    if r<5:
+        ball=Ball()
+        balls.append(ball)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -53,6 +88,10 @@ while running:
 
     screen.fill(background_color)
 
+    for ball in balls:
+        ball.update()
+        ball.draw()
+
     for game in games:
         button_rect = pygame.Rect(
             (screen_width - button_width) // 2,
@@ -67,5 +106,6 @@ while running:
         screen.blit(text_surface, text_rect)
 
     pygame.display.flip()
+    clock.tick(60)
 
 pygame.quit()
