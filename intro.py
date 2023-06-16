3e6a88a385f9ecb06ba73020ce3d31ab39d06c3c
aekmekci72 <anna.ekmekci@gmail.com>
2023-06-16 15:38:50 -0400

    small updates

--- intro.py
+++ intro.py
@@ -21,28 +21,6 @@ height = screen.get_height()
 splash_page = pygame.image.load('images/games.png')
 scaled_splash = pygame.transform.scale(splash_page, (800, 800))
 
-games = [
-    {"name": "Endless Runner", "path": "endless_runner.py"},
-    {"name": "Tic Tac Toe", "path": "tic_tac_toe.py"},
-    {"name": "Space Invaders", "path": "space_invaders.py"},
-    {"name": "Pong", "path": "pong.py"},
-    {"name": "Flappy Bird", "path": "flappy_bird.py"},
-    {"name": "Clicker Simulator", "path": "clicker.py"},
-    {"name": "Snake", "path": "snakeworking.py"},
-    {"name": "Car Race", "path": "race.py"},
-    
-]
-
-button_width = 200
-button_height = 50
-button_padding = 20
-button_start_y = (screen_height - (len(games) * (button_height + button_padding))) // 2
-
-background_color = (225,225,225)
-button_color = (0, 0, 0)
-text_color = (225,225, 225)
-
-
 class Ball:
     def __init__(self):
         self.radius = 20
@@ -87,30 +65,28 @@ while running:
             freq+=1
             if event.button == 1:
                 mouse_pos = pygame.mouse.get_pos()
-                print(mouse_pos)
-                print(mouse_pos)
-                if mouse_pos[0]>475 and mouse_pos[0]<860 and mouse_pos[1]>310 and mouse_pos[1]<415:
+                if mouse_pos[0]>490 and mouse_pos[0]<850 and mouse_pos[1]>305 and mouse_pos[1]<400:
                     subprocess.Popen("python endless_runner.py")
                     pygame.quit()
-                if mouse_pos[0]>475 and mouse_pos[0]<860 and mouse_pos[1]>475 and mouse_pos[1]<575:
+                if mouse_pos[0]>490 and mouse_pos[0]<850 and mouse_pos[1]>450 and mouse_pos[1]<545:
                     subprocess.Popen("python tic_tac_toe.py")
                     pygame.quit()
-                if mouse_pos[0]>475 and mouse_pos[0]<860 and mouse_pos[1]>645 and mouse_pos[1]<745:
+                if mouse_pos[0]>490 and mouse_pos[0]<850 and mouse_pos[1]>610 and mouse_pos[1]<700:
                     subprocess.Popen("python space_invaders.py") 
                     pygame.quit()
-                if mouse_pos[0]>900 and mouse_pos[0]<1280 and mouse_pos[1]>125 and mouse_pos[1]<225:
+                if mouse_pos[0]>900 and mouse_pos[0]<1260 and mouse_pos[1]>135 and mouse_pos[1]<230:
                     subprocess.Popen("python pong.py")
                     pygame.quit()
-                if mouse_pos[0]>900 and mouse_pos[0]<1280 and mouse_pos[1]>260 and mouse_pos[1]<360:
+                if mouse_pos[0]>900 and mouse_pos[0]<1260 and mouse_pos[1]>265 and mouse_pos[1]<350:
                     subprocess.Popen("python flappy_bird.py")
                     pygame.quit()
-                if mouse_pos[0]>900 and mouse_pos[0]<1280 and mouse_pos[1]>390 and mouse_pos[1]<490:
+                if mouse_pos[0]>900 and mouse_pos[0]<1260 and mouse_pos[1]>380 and mouse_pos[1]<470:
                     subprocess.Popen("python clicker.py")
                     pygame.quit()
-                if mouse_pos[0]>900 and mouse_pos[0]<1280 and mouse_pos[1]>520 and mouse_pos[1]<620:
+                if mouse_pos[0]>900 and mouse_pos[0]<1260 and mouse_pos[1]>495 and mouse_pos[1]<585:
                     subprocess.Popen("python snakeworking.py")
                     pygame.quit()
-                if mouse_pos[0]>900 and mouse_pos[0]<1280 and mouse_pos[1]>650 and mouse_pos[1]<750:
+                if mouse_pos[0]>900 and mouse_pos[0]<1260 and mouse_pos[1]>610 and mouse_pos[1]<700:
                     subprocess.Popen("python race.py")
                     pygame.quit()
                 
@@ -119,25 +95,7 @@ while running:
             if event.key == pygame.K_q:
                 running=False
 
-    screen.fill(background_color)
-
-    for ball in balls:
-        ball.update()
-        ball.draw()
-
-    for game in games:
-        button_rect = pygame.Rect(
-            (screen_width - button_width) // 2,
-            button_start_y + games.index(game) * (button_height + button_padding),
-            button_width,
-            button_height
-        )
-        pygame.draw.rect(screen, button_color, button_rect)
-
-        text_surface = font.render(game["name"], True, text_color)
-        text_rect = text_surface.get_rect(center=button_rect.center)
-        screen.blit(text_surface, text_rect)
-
+    
     scaled_splash = pygame.transform.smoothscale(scaled_splash, (width, height))
 
     screen.blit(scaled_splash, (0, 0))
